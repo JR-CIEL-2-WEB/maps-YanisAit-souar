598fd2fd342f9ac60652bf129de4daaa3408ed1f
YanisAit-souar <yanis.ait-souar@lycee-jeanrostand.fr>
2025-09-30 14:48:23 +0000

    Personnalisation du fichier JSON de markers

--- app1/maps.js
+++ app1/maps.js
@@ -1,52 +1,46 @@
-// Coordonnées GPS pour Villepinte
-const villepinteLocations = [
-  ["Lycee Jean Rostand", 48.959136, 2.545756, 1, "Lycee public Jean Rostand"],
-  ["Point 2", 48.960916, 2.542787, 1, "Point 2 "],
-  ["Point 3", 48.959240, 2.548237, 1, "Point 3 "],
-  ["Point 4", 48.959803, 2.548774, 1, "Point 4 "],
-  ["Point 5", 48.961606, 2.553237, 1, "Point 5 "],
-  ["Point 6", 48.963382, 2.550126, 1, "Point 6 "]
-];
-
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
-    center: { lat: 48.9580, lng: 2.5450 },  
+    center: { lat: 48.9580, lng: 2.5450 },  // Position centrale de Villepinte
   });
 
-  setMarkers(map);
+  
+  fetch('markersJrostand.json')
+    .then(response => response.json())  
+    .then(data => {
+      setMarkers(map, data);  
+    })
+    .catch(error => console.error('Erreur de chargement du fichier JSON:', error));
 }
 
-
-function setMarkers(map) {
+function setMarkers(map, markersData) {
   const icon = {
-    url: "https://maps.google.com/mapfiles/ms/icons/red-dot.png",  
-    scaledSize: new google.maps.Size(32, 32),  
+    url: "https://maps.google.com/mapfiles/ms/icons/red-dot.png",
+    scaledSize: new google.maps.Size(32, 32), 
   };
 
-  const infowindow = new google.maps.InfoWindow();
+  const infowindow = new google.maps.InfoWindow(); 
 
-  for (let i = 0; i < villepinteLocations.length; i++) {
-    const location = villepinteLocations[i];
-    
+  markersData.forEach(markerData => {
     const marker = new google.maps.Marker({
-      position: { lat: location[1], lng: location[2] },
+      position: { lat: markerData.lat, lng: markerData.lng },
       map: map,
-      icon: icon,  
-      title: location[0],
-      zIndex: location[3],
+      icon: icon,
+      title: markerData.name,
     });
 
     marker.addListener("click", function () {
-      infowindow.setContent(`
+      const contentString = `
         <div>
-          <h3>${location[0]}</h3>
-          <p>${location[4]}</p> <!-- Description du marqueur -->
+          <h3>${markerData.name}</h3>
+          <p><strong>ID du marqueur : </strong>${markerData.id}</p>
+          <p>${markerData.description}</p>
         </div>
-      `);
+      `;
+      infowindow.setContent(contentString);
       infowindow.open(map, marker); 
     });
-  }
+  });
 }
 
 window.initMap = initMap;
